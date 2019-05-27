61c5c3617ee3f2759800896af5445523af4d8cc9
michzhou <michzhou@umich.edu>
2019-05-27 14:53:19 -0400

    initial commit

---
+++ js/animation.js
@@ -0,0 +1,8 @@
+new WOW().init();
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
+});
